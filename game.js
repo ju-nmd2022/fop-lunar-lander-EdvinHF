a8375fde39c908f23a642228cc841b6bfde8fab6
EdvinHF <Hoed22rl@student.ju.se>
2023-02-25 21:54:32 +0100

    restart

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
+let isGameActive = true;
+let isGameWon = false;
+let isGameLost = false;
+let rocketY = 20;
+let velocity = 0.1;
+let acceleration = 0.07;
+let rocketX = 280;
+let button;
+
 createCanvas(600, 500);
 background(255,255,255);
 function rocket(x, y)  {
@@ -12,6 +21,7 @@ function rocket(x, y)  {
 function space(){
     background(115, 147, 179);
 }
+
 function won(x, y){
     push();
     translate(0, 0);
@@ -19,11 +29,36 @@ function won(x, y){
     pop();
 }
 
-let isGameActive = true;
-let rocketY = 20;
-let velocity = 0.1;
-let acceleration = 0.07;
-let rocketX = 280;
+function lost(x, y){
+    push();
+    translate(0, 0);
+    text('Slow down cowboy! You lost!',230 , 250 );
+    pop();
+}
+
+function again() {
+        
+      text('Press R to restart',260, 300); 
+
+      }
+      
+
+function restart(){
+isGameActive = true;
+isGameLost = false;
+isGameWon = false;
+rocketY = 20;
+velocity = 0.1;
+acceleration = 0.07;
+rocketX = 280;
+
+
+
+
+
+}
+   
+
 function draw(){
     space();
     rocket(rocketX, rocketY);
@@ -34,6 +69,7 @@ if(isGameActive){
 
     rocketY = rocketY + velocity;
     velocity = velocity + acceleration;
+    
     moveSideWays = velocity/3;
     if(velocity < 0){
         moveSideWays = velocity * -1;
@@ -45,21 +81,42 @@ if(isGameActive){
         
     }
     if (keyIsDown(LEFT_ARROW)){
-        rocketX = rocketX  - moveSideWays;
+        rocketX = rocketX  - moveSideWays/2;
         
     }
     if (keyIsDown(RIGHT_ARROW)){
-        rocketX = rocketX + moveSideWays;
+        rocketX = rocketX + moveSideWays/2;
     }
-    if (rocketY > 460 && velocity < 4){
-        //isGameActive = false;
-        
-         
-            won();
-    
+    if (rocketY > 460 && velocity < 1){
+        isGameActive = false;
+        isGameWon = true;
 
     }
+    if(rocketY > 460 && velocity >= 1){
+        isGameActive = false;
+        isGameLost = true;
+    }
+   
+  
 }
+else{
+if (isGameWon){
+    won();
+    again();
+    if(keyIsDown(82)){ 
+     restart();
+    }
+}else {
+    lost();
+    again();
+    if(keyIsDown(82)){ 
+        restart();
+       }
+    
+}
+}
+
+
 }
 
 
