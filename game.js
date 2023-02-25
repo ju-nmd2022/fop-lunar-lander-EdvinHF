18d8c156bd81e5aed700f7ddcba2eba459dc97b7
EdvinHF <Hoed22rl@student.ju.se>
2023-02-25 16:40:53 +0100

    left/right win/lose

--- game.js
+++ game.js
@@ -8,11 +8,18 @@ function rocket(x, y)  {
     pop();
 }
 
+
 function space(){
     background(115, 147, 179);
 }
+function won(x, y){
+    push();
+    translate(0, 0);
+    text('Victory Royal! You have landed succesfully! ',200 , 250 );
+    pop();
+}
 
-
+let isGameActive = true;
 let rocketY = 20;
 let velocity = 0.1;
 let acceleration = 0.07;
@@ -20,11 +27,39 @@ let rocketX = 280;
 function draw(){
     space();
     rocket(rocketX, rocketY);
+   
+
+
+if(isGameActive){
+
     rocketY = rocketY + velocity;
     velocity = velocity + acceleration;
-    if (keyIsDown(UP_ARROW)){
-        rocketY = rocketY + 1;
+    moveSideWays = velocity/3;
+    if(velocity < 0){
+        moveSideWays = velocity * -1;
     }
+    
+
+    if (keyIsDown(UP_ARROW) || keyIsDown(87)){
+        velocity = velocity - 0.15;
+        
+    }
+    if (keyIsDown(LEFT_ARROW)){
+        rocketX = rocketX  - moveSideWays;
+        
+    }
+    if (keyIsDown(RIGHT_ARROW)){
+        rocketX = rocketX + moveSideWays;
+    }
+    if (rocketY > 460 && velocity < 4){
+        //isGameActive = false;
+        
+         
+            won();
+    
+
+    }
+}
 }
 
 
---
+++ win_lose.js
@@ -0,0 +1,7 @@
+function won(x, y){
+    push();
+    translate(0, 0);
+text('Victory Royal! You have landed succesfully ',0 , 0 );
+pop();
+}
+won(width/2, height/2);
